7e27ce66c7c8108c512c4c54dbef3bc86a121a7e
Limão <dev@lmnice.me>
2022-02-05 22:58:19 -0300

    M6: prototype functions on NTNF

--- finance_models/br_sovereign_debt.py
+++ finance_models/br_sovereign_debt.py
@@ -8,4 +8,48 @@ from . import tools
 
 class NTNF:
     def __init__(*args, **kwargs):
-        print('Hello!')
+        pass
+    
+    def calcula_prazo(self,
+        dt_inicio,
+        dt_fim,
+        feriados,
+        convencao,
+    ):
+        prazo_anualizado = 0.0
+        return prazo_anualizado
+
+    def constroi_fluxo(self,
+        dt_fim, frequencia
+    ):
+        lista_datas_fluxos = []
+        return lista_datas_fluxos
+
+    def calcula_pu(self,
+        vf, 
+        prazo_anual, 
+        taxa_anual,
+    ):
+        pu = 0.0
+        return pu
+
+    def calcula_taxa_anual(self,
+        pu, 
+        prazo_anual,
+        valor_base = 100,
+    ):
+        taxa_anual = 0.0
+        return taxa_anual
+
+    def calcula_pu_ntnf(self,
+        dt_venc,
+        dt_base,
+        tir,
+    ):
+        cashflow = pd.DataFrame([],
+            columns = ['data', 'vf', 'du', 'fator_desconto', 'pu']
+        )
+        display(cashflow)
+
+        pu = 0.0
+        return pu
--- run.py
+++ run.py
@@ -4,11 +4,17 @@
 #%%
 # imports
 
+# pip install git+ssh://git@github.com:flimao/financial-product-models.git
+
 import datetime as dt
 import numpy as np
 import pandas as pd
 from finance_models.br_sovereign_debt import NTNF
+from finance_models import tools
 
 #%%
 # test
 x = NTNF()
+
+holidays = tools.get_holidays_anbima()
+# %%

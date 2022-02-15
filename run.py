5d232f034744602931db3aa62830dee400d009ad
Felipe Oliveira <flimao@petrobras.com.br>
2022-02-14 22:14:21 -0300

    Include historical data in validation

--- run.py
+++ run.py
@@ -11,7 +11,9 @@ import numpy as np
 import pandas as pd
 from finance_models.br_sovereign_debt_securities import Prefixado
 from finance_models import tools
+import warnings
 
+warnings.filterwarnings('ignore')
 #%%
 # CONSTS
 HISTBRDEBTSEC = r'https://www.tesourotransparente.gov.br/ckan/dataset/df56aa42-484a-4a59-8184-7676580c81e3/resource/796d2059-14e9-44e3-80c9-2d9e30b405c1/download/PrecoTaxaTesouroDireto.csv'
@@ -40,17 +42,65 @@ def brdebt_change_types(df):
     df = df.copy()
 
     df['security_type'] = df['security_type'].astype('category')
-    #for
+    
+    for datecol in ['maturity_date', 'base_date']:
+        df[datecol] = pd.to_datetime(df[datecol], dayfirst = True)
+        df[datecol] = df[datecol].dt.date
+    
+    for col in ['rate_buy', 'rate_sell', 'pu_buy', 'pu_sell', 'pu_base' ]:
+        df[col] = df[col].str.replace('.', '').str.replace(',', '.').astype(float)
 
     return df
 
-#brdebt_raw = pd.read_csv(HISTBRDEBTSEC, sep=';')
+brdebt_raw = pd.read_csv(HISTBRDEBTSEC, sep=';')
+brdebt = (brdebt_raw
+    .pipe(brdebt_rename_cols)
+    .pipe(brdebt_change_types)
+)
 
 #%%
 # test
 
 holidays = tools.get_holidays_anbima()
 
+ntnf_hist_full = brdebt[brdebt['security_type'] == 'Tesouro Prefixado com Juros Semestrais'].sort_values('base_date')
+ntnf_hist = ntnf_hist_full.sample(1).iloc[0]
+
+ntnf_dados = {}
+ntnf_dados['vencimento'] = ntnf_hist['maturity_date']
+ntnf_dados['taxa_anual'] = ntnf_hist['rate_buy'] / 100
+ntnf_dados['taxa_cupom'] = True
+
+ntnfX = Prefixado(**ntnf_dados)
+
+# pu_buy : taxa buy, D+1 a partir da data base
+pu_buy, _ = ntnfX.calcula_pu_ntnf(
+    dt_base = ntnf_hist['base_date'] + pd.offsets.BDay(1),
+    tir = ntnf_hist['rate_buy']/100
+)
+pu_buy_expect = ntnf_hist['pu_buy']
+
+# pu_sell: taxa sell, D+1 a partir da data base
+pu_sell, _ = ntnfX.calcula_pu_ntnf(
+    dt_base = ntnf_hist['base_date'] + pd.offsets.BDay(1),
+    tir = ntnf_hist['rate_sell']/100
+)
+pu_sell_expect = ntnf_hist['pu_sell']
+
+# pu_base: taxa sell, D+0 da data base
+pu_base, _ = ntnfX.calcula_pu_ntnf(
+    dt_base = ntnf_hist['base_date'],
+    tir = ntnf_hist['rate_sell']/100
+)
+pu_base_expect = ntnf_hist['pu_base']
+
+print(f"PUs de {ntnfX} (data base {ntnf_hist['base_date']:%d/%m/%Y}):")
+print(f'     PU Buy (D+1): esperado {tools.Money(pu_buy_expect)}, retornou {tools.Money(pu_buy)}')
+print(f'    PU Sell (D+1): esperado {tools.Money(pu_sell_expect)}, retornou {tools.Money(pu_sell)}')
+print(f'    PU Base (D+0): esperado {tools.Money(pu_base_expect)}, retornou {tools.Money(pu_base)}')
+
+#%%
+# titulos prefixados em 12/02/2022
 ntnf31 = Prefixado(
     vencimento = 2031,
     taxa_anual = 11.54/100,
